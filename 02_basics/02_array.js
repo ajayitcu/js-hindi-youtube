d6993e065fd69f5e45fb3ee7014647c019c196ca
Ajay Kumar <113685305+ajayitcu@users.noreply.github.com>
2023-11-28 09:46:09 +0000

    folder added

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -24,6 +24,7 @@ console.log(Array.isArray("Hitesh"))
 console.log(Array.from("Hitesh"))
 console.log(Array.from({name: "hitesh"})) // interesting
 
+
 let score1 = 100
 let score2 = 200
 let score3 = 300
